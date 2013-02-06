e1b6d023d5e8e3443bb53e9afd51185978f89444
Nicolas Tisserand <nt@manctl.com>
2013-02-06 18:13:00 +0100

    Import and trim mansrc/boot/cbuild.

---
+++ remake.cmake
@@ -0,0 +1,311 @@
+macro(append l first) # ...
+    list(APPEND ${l} ${first} ${ARGN})
+endmacro()
+
+macro(subdirs firstDir) # ...
+    foreach(dir ${firstDir} ${ARGN})
+        add_subdirectory(${dir})
+    endforeach()
+endmacro()
+
+macro(debug_var var)
+    message("${var}=${${var}}")
+endmacro()
+
+macro(set_global var val)
+    set_property(GLOBAL PROPERTY ${var} ${val})
+endmacro()
+
+macro(get_global var)
+    get_property(${var} GLOBAL PROPERTY ${var})
+endmacro()
+
+macro(append_global var first) # second ... last
+    get_global(${var})
+    list(APPEND ${var} ${first} ${ARGN})
+    set_global(${var} ${${var}})
+endmacro()
+
+# Simplify the cross-platform model.
+if(WIN32)
+    set(WINDOWS TRUE)
+    append_global(PLATFORM_DEFS "SYS_WINDOWS")
+elseif(UNIX)
+    append_global(PLATFORM_DEFS "SYS_UNIX")
+    if(APPLE)
+        set(MACOSX TRUE)
+        append_global(PLATFORM_DEFS "SYS_MACOSX")
+    else()
+        set(LINUX TRUE)
+        append_global(PLATFORM_DEFS "SYS_LINUX")
+    endif()
+endif()
+
+# Beautify IDE projects.
+set_global(USE_FOLDERS ON)
+set_global(PREDEFINED_TARGETS_FOLDER "[CMAKE]")
+
+# Put sources in folders.
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+    macro(folderify_sources src0) # ...
+        source_group("Output" REGULAR_EXPRESSION "\\.rule$")
+        foreach(src ${src0} ${ARGN})
+            get_filename_component(folder ${src} PATH)
+            if(NOT folder)
+                set(folder "/")
+            elseif(IS_ABSOLUTE ${folder})
+                string(REGEX REPLACE "^${CMAKE_CURRENT_BINARY_DIR}.*" "Output" folder ${folder})
+                string(REGEX REPLACE "^${CMAKE_CURRENT_SOURCE_DIR}/?" "/" folder ${folder})
+            else()
+                set(folder "/${folder}")
+            endif()
+            # message("${src} => ${folder}")
+            source_group(${folder} FILES ${src})
+        endforeach()
+    endmacro()
+else()
+    macro(folderify_sources src0) # ...
+    endmacro()
+endif()
+
+if(APPLE OR CMAKE_COMPILER_IS_GNUC)
+    set(CBUILD_STRICT_C_FLAGS   "-Wall -Wextra -Werror -ansi -std=c99   -pedantic -Wno-variadic-macros -Wno-long-long -Wno-unknown-pragmas")
+endif()
+
+if(APPLE OR CMAKE_COMPILER_IS_GNUCXX)
+    set(CBUILD_STRICT_CXX_FLAGS "-Wall -Wextra -Werror -ansi -std=c++98 -pedantic -Wno-variadic-macros -Wno-long-long -Wno-unknown-pragmas")
+endif()
+
+macro(dirname var path)
+    get_filename_component(${var} ${path} PATH)
+endmacro()
+
+macro(target target_)
+    project(${target_})
+    set(HERE_BIN ${CMAKE_CURRENT_BINARY_DIR})
+    set(HERE     ${CMAKE_CURRENT_SOURCE_DIR})
+    include_directories(${HERE_BIN} ${HERE})
+endmacro()
+
+macro(register_target_files target KIND kind)
+    set(${kind}_files_h ${CMAKE_CURRENT_BINARY_DIR}/${target}-${kind}-files.h)
+    dir2code(${${kind}_files_h} ${${target}_${KIND}})
+    set(${kind}_files_cpp ${CMAKE_CURRENT_BINARY_DIR}/${target}-${kind}-files.cpp)
+    dir2code(${${kind}_files_cpp} ${${target}_${KIND}})
+    append(${target}_SOURCES
+        ${${kind}_files_h}
+        ${${kind}_files_cpp}
+    )
+    append(${target}_GENERATED
+        ${${kind}_files_h}
+        ${${kind}_files_cpp}
+    )
+endmacro(register_target_files)
+
+macro(pre_target target)
+    if(${target}_UNITS)
+        foreach(unit ${${target}_UNITS})
+            append(${target}_SOURCES
+                ${unit}.cpp
+                ${unit}.h
+            )
+        endforeach()
+    endif()
+    if(${target}_HPP_UNITS)
+        foreach(unit ${${target}_HPP_UNITS})
+            append(${target}_SOURCES
+                ${unit}.cpp
+                ${unit}.h
+                ${unit}.hpp
+            )
+        endforeach()
+    endif()
+    if(${target}_MOC_UNITS)
+        foreach(unit ${${target}_MOC_UNITS})
+            append(${target}_MOC ${unit}.h)
+            append(${target}_SOURCES
+                ${unit}.cpp
+                ${unit}.h
+            )
+        endforeach()
+    endif()
+    if(${target}_UIC_UNITS)
+        foreach(unit ${${target}_UIC_UNITS})
+            append(${target}_UIC ${unit}.ui)
+            append(${target}_MOC ${unit}.h)
+            append(${target}_SOURCES
+                ${unit}.cpp
+                ${unit}.h
+            )
+        endforeach()
+    endif()
+    if(${target}_MOC)
+        QT4_WRAP_CPP(${target}_MOC_SOURCES ${${target}_MOC})
+        append(${target}_SOURCES ${${target}_MOC_SOURCES})
+    endif()
+    if(${target}_JS)
+        set(js_qrc ${CMAKE_CURRENT_BINARY_DIR}/${target}-js.qrc)
+        dir2code(${js_qrc} ${${target}_JS})
+        append(${target}_QRC ${js_qrc})
+        append(${target}_GENERATED ${js_qrc})
+        append(${target}_FILES ${${target}_JS})
+        register_target_files(${target} JS js)
+    endif()
+    if(${target}_QML)
+        set(qmldir ${CMAKE_CURRENT_BINARY_DIR}/${target}-qmldir)
+        dir2code(${qmldir} ${${target}_QML})
+        set(qml_qrc ${CMAKE_CURRENT_BINARY_DIR}/${target}-qml.qrc)
+        dir2code(${qml_qrc} ${${target}_QML} ${qmldir})
+        append(${target}_QRC ${qml_qrc})
+        append(${target}_GENERATED ${qml_qrc} ${qmldir})
+        append(${target}_FILES ${${target}_QML} ${qmldir})
+        register_target_files(${target} QML qml)
+    endif()
+    if(${target}_GFX)
+        set(gfx_qrc ${CMAKE_CURRENT_BINARY_DIR}/${target}-gfx.qrc)
+        dir2code(${gfx_qrc} ${${target}_GFX})
+        append(${target}_QRC ${gfx_qrc})
+        append(${target}_GENERATED ${gfx_qrc})
+        append(${target}_FILES ${${target}_GFX})
+        register_target_files(${target} GFX gfx)
+    endif()
+    if(${target}_QRC)
+        QT4_ADD_RESOURCES(${target}_QRC_SOURCES ${${target}_QRC})
+        append(${target}_SOURCES ${${target}_QRC} ${${target}_QRC_SOURCES})
+    endif()
+    if(${target}_UIC)
+        QT4_WRAP_UI(${target}_UIC_SOURCES ${${target}_UIC})
+        append(${target}_SOURCES ${${target}_UIC} ${${target}_UIC_SOURCES})
+    endif()
+    if(WIN32 AND ${target}_WIN32_RC)
+        append(${target}_SOURCES ${${target}_WIN32_RC})
+    endif()
+    if(${target}_FILES)
+        foreach(file ${${target}_FILES})
+            append(${target}_SOURCES ${file})
+        endforeach()
+    endif()
+    folderify_sources(${${target}_SOURCES})
+endmacro()
+
+macro(get_source_language src var)
+    if(src MATCHES "\\.c$")
+        set(${var} C)
+    elseif(src MATCHES "\\.cpp$")
+        set(${var} CXX)
+    else()
+        set(${var})
+    endif()
+endmacro()
+
+macro(post_target target)
+    if(${target}_STRICT)
+        foreach(src ${${target}_SOURCES})
+            get_source_language(${src} src_lang)
+            if(src_lang STREQUAL "C" AND DEFINED CBUILD_STRICT_C_FLAGS)
+                set_property(SOURCE ${src} APPEND PROPERTY COMPILE_FLAGS ${CBUILD_STRICT_C_FLAGS})
+            elseif(src_lang STREQUAL "CXX" AND DEFINED CBUILD_STRICT_CXX_FLAGS)
+                set_property(SOURCE ${src} APPEND PROPERTY COMPILE_FLAGS ${CBUILD_STRICT_CXX_FLAGS})
+            endif()
+        endforeach()
+    endif()
+    if(${target}_FILENAME)
+        set_property(TARGET ${target} PROPERTY OUTPUT_NAME ${${target}_FILENAME})
+    endif()
+    foreach(dep ${${target}_DEPS})
+        run_dep(${target} ${dep})
+    endforeach()
+    if(${target}_PLATFORM_DEFS)
+        get_global(${PLATFORM_DEFS})
+        set_property(TARGET ${target} PROPERTY COMPILE_DEFINITIONS ${PLATFORM_DEFS})
+    endif()
+    if(${target}_FILES)
+        foreach(file ${${target}_FILES})
+            set_property(SOURCE ${file} PROPERTY HEADER_FILE_ONLY TRUE)
+        endforeach()
+    endif()
+    if(${target}_GENERATED)
+        foreach(file ${${target}_GENERATED})
+            append(${target}_SOURCES ${file})
+            set_property(SOURCE ${file} PROPERTY GENERATED TRUE)
+        endforeach()
+    endif()
+endmacro()
+
+macro(link_platforms target first_platform) # ...
+    foreach(platform ${first_platform} ${ARGN})
+        if(${platform} AND ${target}_LIBS_${platform})
+            target_link_libraries(${target} ${${target}_LIBS_${platform}})
+        endif()
+    endforeach()
+endmacro()
+
+macro(link target)
+    link_platforms(${target}
+        WINDOWS
+        MACOSX
+        LINUX
+        UNIX
+    )
+
+    if(${target}_LIBS)
+        target_link_libraries(${target} ${${target}_LIBS})
+    endif()
+
+    if(${target}_LIBS_DEBUG)
+        target_link_libraries(${target} debug ${${target}_LIBS_DEBUG})
+    endif()
+
+    if(${target}_LIBS_OPTIMIZED)
+        target_link_libraries(${target} optimized ${${target}_LIBS_OPTIMIZED})
+    endif()
+endmacro()
+
+macro(build_executable target) # ...
+    pre_target(${target})
+    add_executable(${target} ${ARGN}
+        ${${target}_SOURCES}
+    )
+    post_target(${target})
+    link(${target})
+    set_property(TARGET ${target} PROPERTY FOLDER "exes")
+endmacro()
+
+macro(build_application target) # ...
+    # FIXME: Add WIN32 & proper dispatch dispatch between:
+    #   * Qt's WinMain for Qt applications,
+    #   * and /ENTRY:mainCRTStartup linker flag for non-qt applications.
+    build_executable(${target} MACOSX_BUNDLE ${ARGN})
+    set_property(TARGET ${target} PROPERTY FOLDER "apps")
+endmacro()
+
+macro(build_module target) # ...
+    pre_target(${target})
+    add_library(${target} MODULE ${ARGN}
+        ${${target}_SOURCES}
+    )
+    post_target(${target})
+    link(${target})
+    set_property(TARGET ${target} PROPERTY FOLDER "mods")
+endmacro()
+
+macro(build_server target)
+    pre_target(${target})
+    add_executable(${target} ${ARGN}
+        ${${target}_SOURCES}
+    )
+    post_target(${target})
+    link(${target})
+    set_property(TARGET ${target} PROPERTY FOLDER "webs")
+endmacro()
+
+macro(build_static_library target)
+    pre_target(${target})
+    add_library(${target} STATIC
+        ${${target}_SOURCES}
+    )
+    post_target(${target})
+    link(${target})
+    set_property(TARGET ${target} PROPERTY FOLDER "libs")
+    register_library_dep(${target})
+endmacro()
