1e9d9ec0fb233b4747fd0c75c39e6bbc9245b466
Nicolas Burrus <nicolas.burrus@manctl.com>
2013-04-03 19:38:30 +0200

    Fix issue on Windows when PLATFORM_DEFS is not defined.

--- mods/platform-post-target.cmake
+++ mods/platform-post-target.cmake
@@ -1,6 +1,8 @@
 if(${T}_PLATFORM_DEFS)
-    get_global(${PLATFORM_DEFS})
-    set_property(TARGET ${T} APPEND PROPERTY COMPILE_DEFINITIONS
-        ${PLATFORM_DEFS}
-    )
+    if(DEFINED PLATFORM_DEFS)
+		get_global(${PLATFORM_DEFS})
+		set_property(TARGET ${T} APPEND PROPERTY COMPILE_DEFINITIONS
+			${PLATFORM_DEFS}
+		)
+	endif()
 endif()
